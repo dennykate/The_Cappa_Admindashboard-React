569794a799a2d49942ab00ee7a1e3df7628ee8c3
thwe thwe tun <thwethwehtun2000@gmail.com>
2023-06-16 14:59:12 +0630

    some route changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-
 import { Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout";
+
 import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
-    </Layout>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+    </Routes>
   );
 };
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,18 @@
 import { Badge, Box, Button, Text } from "@mantine/core";
 import React from "react";
+import Layout from "../components/Layout";
 
 const Dashboard = () => {
   return (
-    <Box>
-      <Text>Dashboard</Text>
-      <Button color="ocean-blue">Ocean blue button</Button>
-      <Badge color="bright-pink" variant="filled">
-        Bright pink badge
-      </Badge>
-    </Box>
+    <Layout>
+      <Box>
+        <Text>Dashboard</Text>
+        <Button color="ocean-blue">Ocean blue button</Button>
+        <Badge color="bright-pink" variant="filled">
+          Bright pink badge
+        </Badge>
+      </Box>
+    </Layout>
   );
 };
 
