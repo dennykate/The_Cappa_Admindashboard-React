f435426cd1e57e8dc375c3909297f78922ead34d
Denny_Kate <dennykate22@gmail.com>
2023-06-17 21:43:20 +0630

    Grid Card adding

--- src/components/DataCard.jsx
+++ src/components/DataCard.jsx
@@ -1,4 +1,11 @@
-import { Box, Card, Flex, Grid, useMantineColorScheme } from "@mantine/core";
+import {
+  Box,
+  Card,
+  Flex,
+  Grid,
+  Text,
+  useMantineColorScheme,
+} from "@mantine/core";
 import React from "react";
 import { IoBedOutline } from "react-icons/io5";
 
@@ -7,20 +14,30 @@ const DataCard = () => {
   const dark = colorScheme === "dark";
 
   return (
-    <Grid.Col span={3}>
+    <Grid.Col md={6} lg={3}>
       <Card
-        shadow="md"
         padding="lg"
-        className="group bg-white flex items-center gap-[5px] p-[20px] rounded-[10px]"
+        className="group bg-white flex items-center gap-[15px] p-[20px] rounded-[10px] group shadow-sm
+        hover:shadow-lg transition-all duration-300 ease-in-out"
       >
         <Flex
           justify="center"
           align="center"
-          className="w-[60px] h-[60px] bg-primary bg-opacity-10 rounded-[10px]"
+          className="w-[60px] h-[60px] bg-primary group-hover:bg-opacity-100 bg-opacity-10 rounded-[10px] transition-all duration-300 ease-in-out"
         >
-          <IoBedOutline size={30} className="text-primary" />
+          <IoBedOutline
+            size={32}
+            className="text-primary group-hover:text-white transition-all duration-300 ease-in-out"
+          />
         </Flex>
-        <Flex direction="column"></Flex>
+        <Flex direction="column">
+          <Text fw={700} className="text-[28px]">
+            8,461
+          </Text>
+          <Text className="text-[12px]" fw={"normal"}>
+            New Booking
+          </Text>
+        </Flex>
       </Card>
     </Grid.Col>
   );
