5b9dbd908ad8780b288305ce7aba1e46f35611ba
thwe thwe tun <thwethwehtun2000@gmail.com>
2023-07-02 01:14:49 +0630

    Merged chitchit 2

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -6,7 +6,7 @@ import { cardData } from "../utils/data";
 
 const CardContainer = ({ dark }) => {
   return (
-    <div className="w-full grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-[20px]">
+    <div className=" grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-[20px]">
       {cardData?.map((data, index) => (
         <DataCard key={index} {...data} />
       ))}
