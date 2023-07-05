5276e791a23777233249775bc1d63548e570008c
Denny_Kate <dennykate22@gmail.com>
2023-07-05 22:29:41 +0630

    Added AuthGuard

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,6 +21,7 @@ import AuthHorizontalLine from "../components/AuthHorizontalLine";
 import SocialBtnGroup from "../components/SocialBtnGroup";
 import LogoDarkLight from "../components/LogoDarkLight";
 import Cookies from "js-cookie";
+import AuthGuard from "../components/AuthGuard";
 
 export default function Login() {
   const { ref, focused } = useFocusWithin();
@@ -35,7 +36,8 @@ export default function Login() {
     // functions will be used to validate values at corresponding key
     validate: {
       email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
-      password: (value) => value.length<8 ?"Password must contain at least 8 characters": null
+      password: (value) =>
+        value.length < 8 ? "Password must contain at least 8 characters" : null,
     },
   });
 
@@ -52,93 +54,95 @@ export default function Login() {
   }, [colorScheme]);
 
   return (
-    <Flex
-      align={"center"}
-      justify={"center"}
-      w={"100%"}
-      mih={"100vh"}
-      bg={dark ? "#1B1B1B" : "#F8F5F0"}
-      className="relative"
-    >
-      <div className="logo top-5 absolute flex justify-between items-center w-full lg:px-48 sm:px-10 px-5">
-        <LogoDarkLight dark={dark} />
+    <AuthGuard>
+      <Flex
+        align={"center"}
+        justify={"center"}
+        w={"100%"}
+        mih={"100vh"}
+        bg={dark ? "#1B1B1B" : "#F8F5F0"}
+        className="relative"
+      >
+        <div className="logo top-5 absolute flex justify-between items-center w-full lg:px-48 sm:px-10 px-5">
+          <LogoDarkLight dark={dark} />
 
-        <AuthDarkLightBtn toggleColorScheme={toggleColorScheme} dark={dark} />
-      </div>
+          <AuthDarkLightBtn toggleColorScheme={toggleColorScheme} dark={dark} />
+        </div>
 
-      <Paper sx={{ width: 420 }} bg={"transparent"}>
-        <Title
-          align="center"
-          sx={(theme) => ({
-            fontFamily: "Gilda Display,serif",
-            fontWeight: 900,
-            color: "#AA8453",
-          })}
-          className="sm:text-4xl text-2xl sm:mt-0 mt-10"
-        >
-          Welcome back!
-        </Title>
-        <Text
-          size="sm"
-          align="center"
-          mt={5}
-          className={`${dark ? "text-[#F8F5F0]" : "text-gray-400"}`}
-        >
-          Login with social media or your credentials
-        </Text>
+        <Paper sx={{ width: 420 }} bg={"transparent"}>
+          <Title
+            align="center"
+            sx={(theme) => ({
+              fontFamily: "Gilda Display,serif",
+              fontWeight: 900,
+              color: "#AA8453",
+            })}
+            className="sm:text-4xl text-2xl sm:mt-0 mt-10"
+          >
+            Welcome back!
+          </Title>
+          <Text
+            size="sm"
+            align="center"
+            mt={5}
+            className={`${dark ? "text-[#F8F5F0]" : "text-gray-400"}`}
+          >
+            Login with social media or your credentials
+          </Text>
 
-        <Paper bg={"transparent"} p={30} radius="md" className="form-area">
-          <form onSubmit={form.onSubmit(onSubmitHandler)}>
-            <InputText dark={dark} form={form} />
-            <InputPassword dark={dark} form={form} />
-            <Flex className="justify-between" mt="lg">
-              <Checkbox
-                color="yellow"
-                label="Trust this device for 60 days"
-                classNames={{
-                  label: `sm:text-base text-sm ${
+          <Paper bg={"transparent"} p={30} radius="md" className="form-area">
+            <form onSubmit={form.onSubmit(onSubmitHandler)}>
+              <InputText dark={dark} form={form} />
+              <InputPassword dark={dark} form={form} />
+              <Flex className="justify-between" mt="lg">
+                <Checkbox
+                  color="yellow"
+                  label="Trust this device for 60 days"
+                  classNames={{
+                    label: `sm:text-base text-sm ${
+                      dark ? "text-[#F8F5F0]" : "text-gray-500"
+                    }`,
+                    input: "bg-bgLight border-primary",
+                  }}
+                />
+
+                <Anchor
+                  component="button"
+                  color="#AA8453"
+                  className="text-xs sm:text-sm"
+                >
+                  Forgot password?
+                </Anchor>
+              </Flex>
+              <Button
+                type="submit"
+                fullWidth
+                variant="filled"
+                className="mt-5 bg-[#AA8453] active:bg-opacity-80 hover:bg-[#AA8453] h-12 text-base"
+              >
+                Sign In
+              </Button>
+              <AuthHorizontalLine dark={dark} />
+              <SocialBtnGroup />
+              <div className="flex justify-between items-center mt-3">
+                <Text
+                  className={`sm:text-base text-sm ${
                     dark ? "text-[#F8F5F0]" : "text-gray-500"
-                  }`,
-                  input: "bg-bgLight border-primary",
-                }}
-              />
-
-              <Anchor
-                component="button"
-                color="#AA8453"
-                className="text-xs sm:text-sm"
-              >
-                Forgot password?
-              </Anchor>
-            </Flex>
-            <Button
-              type="submit"
-              fullWidth
-              variant="filled"
-              className="mt-5 bg-[#AA8453] active:bg-opacity-80 hover:bg-[#AA8453] h-12 text-base"
-            >
-              Sign In
-            </Button>
-            <AuthHorizontalLine dark={dark} />
-            <SocialBtnGroup />
-            <div className="flex justify-between items-center mt-3">
-              <Text
-                className={`sm:text-base text-sm ${
-                  dark ? "text-[#F8F5F0]" : "text-gray-500"
-                }`}
-              >
-                Don't have an account?
-              </Text>
-              <Link
-                to={"/register"}
-                className="text-[#AA8453] sm:text-sm text-xs"
-              >
-                Sign Up
-              </Link>
-            </div>
-          </form>
+                  }`}
+                >
+                  Don't have an account?
+                </Text>
+                <Link
+                  to={"/register"}
+                  className="text-[#AA8453] sm:text-sm text-xs"
+                >
+                  Sign Up
+                </Link>
+              </div>
+            </form>
+          </Paper>
         </Paper>
-      </Paper>
-    </Flex>
+      </Flex>
+    </AuthGuard>
   );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -23,6 +23,7 @@ import AuthDarkLightBtn from "../components/AuthDarkLightBtn";
 import AuthHorizontalLine from "../components/AuthHorizontalLine";
 import SocialBtnGroup from "../components/SocialBtnGroup";
 import LogoDarkLight from "../components/LogoDarkLight";
+import AuthGuard from "../components/AuthGuard";
 
 export default function Register() {
   const navigate = useNavigate();
@@ -53,101 +54,106 @@ export default function Register() {
     navigate("/login");
   };
   return (
-    <Flex
-      align={"center"}
-      justify={"center"}
-      w={"100%"}
-      mih={"100vh"}
-      bg={dark ? "#1B1B1B" : "#F8F5F0"}
-      className="relative py-[50px]"
-    >
-      <div className="logo absolute top-5  w-full flex justify-between items-center lg:px-48 sm:px-10 px-5">
-        <LogoDarkLight dark={dark} />
-        <AuthDarkLightBtn toggleColorScheme={toggleColorScheme} dark={dark} />
-      </div>
+    <AuthGuard>
+      <Flex
+        align={"center"}
+        justify={"center"}
+        w={"100%"}
+        mih={"100vh"}
+        bg={dark ? "#1B1B1B" : "#F8F5F0"}
+        className="relative py-[50px]"
+      >
+        <div className="logo absolute top-5  w-full flex justify-between items-center lg:px-48 sm:px-10 px-5">
+          <LogoDarkLight dark={dark} />
+          <AuthDarkLightBtn toggleColorScheme={toggleColorScheme} dark={dark} />
+        </div>
 
-      <Paper sx={{ width: 420 }} bg={"transparent"} className="mt-10">
-        <Title
-          align="center"
-          sx={(theme) => ({
-            fontFamily: "Gilda Display,serif",
-            fontWeight: 900,
-            color: "#AA8453",
-          })}
-          className="sm:text-4xl text-2xl mt-10"
-        >
-          Welcome to Cappa
-        </Title>
-        <Text
-          className={`${dark ? "text-[#F8F5F0]" : "text-gray-400"}`}
-          size="sm"
-          align="center"
-          mt={5}
-        >
-          Let's start by creating you account
-        </Text>
+        <Paper sx={{ width: 420 }} bg={"transparent"} className="mt-10">
+          <Title
+            align="center"
+            sx={(theme) => ({
+              fontFamily: "Gilda Display,serif",
+              fontWeight: 900,
+              color: "#AA8453",
+            })}
+            className="sm:text-4xl text-2xl mt-10"
+          >
+            Welcome to Cappa
+          </Title>
+          <Text
+            className={`${dark ? "text-[#F8F5F0]" : "text-gray-400"}`}
+            size="sm"
+            align="center"
+            mt={5}
+          >
+            Let's start by creating you account
+          </Text>
 
-        <Paper bg={"transparent"} p={30} radius="md">
-          <form onSubmit={form.onSubmit(onSubmitHandler)}>
-            <InputText
-              dark={dark}
-              form={form}
-              placeholder={"John Doe"}
-              label={"Name*"}
-              value={"name"}
-            />
-            <InputText
-              dark={dark}
-              form={form}
-              placeholder={"eg. example@gmail.com"}
-            />
-            <InputPassword dark={dark} form={form} placeholder={"••••••••"} />
-            <InputPassword
-              dark={dark}
-              confirmation
-              form={form}
-              placeholder={"••••••••"}
-            />
-            <Group mt="lg" className="gap-1">
-              <Checkbox
-                color="yellow"
-                label="I accept the"
-                classNames={{
-                  label: `sm:text-base text-sm ${
-                    dark ? "text-[#F8F5F0]" : "text-gray-500"
-                  }`,
-                  input: "bg-bgLight border-primary",
-                }}
+          <Paper bg={"transparent"} p={30} radius="md">
+            <form onSubmit={form.onSubmit(onSubmitHandler)}>
+              <InputText
+                dark={dark}
+                form={form}
+                placeholder={"John Doe"}
+                label={"Name*"}
+                value={"name"}
               />
-              <Anchor className="text-[#AA8453] sm:text-base text-sm">
-                Terms of Service
-              </Anchor>
-            </Group>
-            <Button
-              type="submit"
-              fullWidth
-              variant="filled"
-              className="mt-5 bg-[#AA8453] bg-opacity-90  active:bg-opacity-80 hover:bg-[#AA8453] hover:bg-opacity-100 h-12 text-base transition-all ease-in-out duration-200"
-            >
-              Sign Up
-            </Button>
-            <AuthHorizontalLine dark={dark} />
-            <SocialBtnGroup dark={dark} />
-            <div className="flex justify-between items-center mt-3">
-              <Text
-                className={`sm:text-base text-sm ${
-                  dark ? "text-[#F8F5F0]" : "text-gray-500"
-                }`}
+              <InputText
+                dark={dark}
+                form={form}
+                placeholder={"eg. example@gmail.com"}
+              />
+              <InputPassword dark={dark} form={form} placeholder={"••••••••"} />
+              <InputPassword
+                dark={dark}
+                confirmation
+                form={form}
+                placeholder={"••••••••"}
+              />
+              <Group mt="lg" className="gap-1">
+                <Checkbox
+                  color="yellow"
+                  label="I accept the"
+                  classNames={{
+                    label: `sm:text-base text-sm ${
+                      dark ? "text-[#F8F5F0]" : "text-gray-500"
+                    }`,
+                    input: "bg-bgLight border-primary",
+                  }}
+                />
+                <Anchor className="text-[#AA8453] sm:text-base text-sm">
+                  Terms of Service
+                </Anchor>
+              </Group>
+              <Button
+                type="submit"
+                fullWidth
+                variant="filled"
+                className="mt-5 bg-[#AA8453] bg-opacity-90  active:bg-opacity-80 hover:bg-[#AA8453] hover:bg-opacity-100 h-12 text-base transition-all ease-in-out duration-200"
               >
-                Already have an account?
-              </Text>
-              <Link to={"/login"} className="text-[#AA8453] sm:text-sm text-xs">
-                Sign In
-              </Link>
-            </div>
-          </form>
+                Sign Up
+              </Button>
+              <AuthHorizontalLine dark={dark} />
+              <SocialBtnGroup dark={dark} />
+              <div className="flex justify-between items-center mt-3">
+                <Text
+                  className={`sm:text-base text-sm ${
+                    dark ? "text-[#F8F5F0]" : "text-gray-500"
+                  }`}
+                >
+                  Already have an account?
+                </Text>
+                <Link
+                  to={"/login"}
+                  className="text-[#AA8453] sm:text-sm text-xs"
+                >
+                  Sign In
+                </Link>
+              </div>
+            </form>
+          </Paper>
         </Paper>
-      </Paper>
-    </Flex>
+      </Flex>
+    </AuthGuard>
   );
 }
